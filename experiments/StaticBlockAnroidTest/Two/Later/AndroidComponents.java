26f70308a1ab1cd977aec5cbea3a23b7cf3de068
Mushfiqur Rahman Chowdhury <mushfiqurchowdhury05@gmail.com>
2026-09-14 15:19:39 -0400

    Experiment complete

--- experiments/StaticBlockAnroidTest/Two/Later/AndroidComponents.java
+++ experiments/StaticBlockAnroidTest/Two/Later/AndroidComponents.java
@@ -2,11 +2,6 @@ package org.greenrobot.eventbus.android;
 
 public abstract class AndroidComponents {
 
-    private static final AndroidComponents implementation;
-
-    static {
-        implementation = AndroidDependenciesDetector.isAndroidSDKAvailable()
-            ? AndroidDependenciesDetector.instantiateAndroidComponents()
-            : null;
-    }
+    private static final AndroidComponents implementation=null;
+    
 }
